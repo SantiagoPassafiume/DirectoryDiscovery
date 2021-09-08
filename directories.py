af831452c17c374b12f86591b253ed36e5c30109
Santiago Passafiume <santiagopassafiume@protonmail.com>
2021-09-08 16:03:31 -0400

    round time to finish

--- directories.py
+++ directories.py
@@ -10,18 +10,16 @@ start = time.perf_counter()
 target_url = input("[+] Enter Target URL (With Protocol): ")
 wordlist = input("[+] Enter Wordlist: ")
 save_to_file = input("[+] Do you want to save the output to a file? (yes/no): ")
-threads = int(input("[+] How many threads do you want to use?: "))
+# threads = int(input("[+] How many threads do you want to use?: "))
 
 
-dict = dividing(wordlist, measure_file(wordlist) / threads)
+dict = dividing(wordlist, 1000)
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     count = 0
-    for _ in range(0, threads):
+    for _ in range(0, 5):
         executor.submit(d.discovery, dict[f"dir{count}"], target_url, save_to_file)
         count += 1
 
 finish = time.perf_counter()
-
-print(len(wordlist))
-print(f"Finished in {finish-start, 2} second(s)")
+print(f"Finished in {round(finish-start, 2)} second(s)")
